a1e2ddad92ab62ab8174805a1856094ccf3cab95
LucasGuerraS <lucasgasardo@gmail.com>
2023-09-04 11:47:44 -0300

    Add all project files

---
+++ app/src/main/java/br/com/fiap/myaiteacher/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.com.fiap.myaiteacher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.fiap.myaiteacher.ui.theme.MyAITeacherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyAITeacherTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ) {
+                    Greeting("My AI Teacher Home")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyAITeacherTheme {
+        Greeting("Android")
+    }
+}
