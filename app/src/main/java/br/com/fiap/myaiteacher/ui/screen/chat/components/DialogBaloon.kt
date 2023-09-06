479f7f7580f9aea236ca920a0b0d9866619a5789
LucasGuerraS <lucasgasardo@gmail.com>
2023-09-06 19:36:50 -0300

    remove unused imports

--- app/src/main/java/br/com/fiap/myaiteacher/ui/screen/chat/components/DialogBaloon.kt
+++ app/src/main/java/br/com/fiap/myaiteacher/ui/screen/chat/components/DialogBaloon.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
