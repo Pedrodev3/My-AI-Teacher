dec952f9b876252cfaad97c2a3f6be8f56328c19
LucasGuerraS <lucasgasardo@gmail.com>
2023-09-06 21:17:01 -0300

    cretae more separate components

--- app/src/main/java/br/com/fiap/myaiteacher/ui/screen/chat/ChatScreen.kt
+++ app/src/main/java/br/com/fiap/myaiteacher/ui/screen/chat/ChatScreen.kt
@@ -1,45 +1,21 @@
 package br.com.fiap.myaiteacher.ui.screen.chat
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import br.com.fiap.myaiteacher.R
-import br.com.fiap.myaiteacher.ui.screen.chat.components.DialogBaloon
-import br.com.fiap.myaiteacher.ui.theme.Montserrat
+import br.com.fiap.myaiteacher.ui.screen.chat.components.ColumnChat
+import br.com.fiap.myaiteacher.ui.screen.chat.components.FooterChat
+import br.com.fiap.myaiteacher.ui.screen.chat.components.HeaderChat
 
 @SuppressLint("UnrememberedMutableState")
 @Composable
@@ -47,114 +23,13 @@ fun ChatScreen(navController: NavController, chatScreenViewModel: ChatScreenView
     val configuration = LocalConfiguration.current
     val items by chatScreenViewModel.commentsList.observeAsState(initial = mutableStateListOf())
     val comment by chatScreenViewModel.comment.observeAsState(initial = "")
+    val scrollState = rememberLazyListState()
 
     Column() {
-        Row(
-            modifier = Modifier
-                .background(color = Color(0xFF00002E))
-                .height((configuration.screenHeightDp * 0.11).dp)
-                .width((configuration.screenWidthDp).dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            IconButton(
-                onClick = {
-                    navController.navigate(route = "login")
-                },
-                modifier = Modifier
-                    .width(35.dp)
-                    .height(35.dp)
-            )
-            {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_keyboard_backspace_24),
-                    contentDescription = "Return to login",
-                    tint = Color(0xB2FFFFFF)
-                )
-            }
-            Text(
-                modifier = Modifier.width(248.dp),
-                text = "Hey exemplo, I am your AI teacher and I am here to help!",
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    fontFamily = Montserrat,
-                    fontWeight = FontWeight(400),
-                    color = Color(0xFFFFFFFF),
-                    textAlign = TextAlign.Center
-                )
-            )
-            Image(
-                painter = painterResource(id = R.drawable.aiteacher),
-                contentDescription = "Imagem de um cara",
-                modifier = Modifier
-                    .size(50.dp),
-                contentScale = ContentScale.Crop
-            )
-        }
+        HeaderChat(navController = navController, configuration = configuration)
         Spacer(modifier = Modifier.height((configuration.screenHeightDp * 0.002).dp))
-        LazyColumn(
-            modifier = Modifier
-                .background(color = Color(0xFF2C2F34))
-                .height((configuration.screenHeightDp * 0.758).dp)
-                .fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(space = 56.dp, alignment = Alignment.Bottom),
-            contentPadding = PaddingValues(vertical = 30.dp)
-        ) {
-            items.forEachIndexed { index, item ->
-                item {
-                    DialogBaloon(index = index, text = item)
-                }
-            }
-        }
+        ColumnChat(configuration = configuration, items = items, scrollState = scrollState)
         Spacer(modifier = Modifier.height((configuration.screenHeightDp * 0.001).dp))
-        Row(
-            modifier = Modifier
-                .background(color = Color(0xFF2C2F34))
-                .height((configuration.screenHeightDp * 0.13).dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            OutlinedTextField(
-                value = comment,
-                onValueChange = {
-                                chatScreenViewModel.onCommentChanged(it)
-                },
-                placeholder = {
-                    Text(text = "What’s your question? ",
-                        style = TextStyle(
-                            fontSize = 14.sp,
-                            fontFamily = Montserrat,
-                            fontWeight = FontWeight(400),
-                            color = Color(0xB2FFFFFF),
-                        )
-                    )
-                },
-                modifier = Modifier
-                    .width(287.dp)
-                    .height(55.dp)
-                    .padding(start = 22.dp),
-                shape = RoundedCornerShape(
-                    12.dp
-                ),
-                keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences)
-            )
-            IconButton(
-                onClick = {
-                    chatScreenViewModel.onAddNewComment(comment)
-                    chatScreenViewModel.onCommentChanged("")
-                },
-                modifier = Modifier
-                    .padding(start = 30.dp, end = 10.dp)
-                    .width(35.dp)
-                    .height(35.dp)
-            )
-            {
-                Icon(
-                    painter = painterResource(id = R.drawable.baseline_send_24),
-                    contentDescription = "Send message",
-                    tint = Color(0xB2FFFFFF)
-                )
-            }
-        }
+        FooterChat(configuration = configuration, comment = comment, chatScreenViewModel = chatScreenViewModel, items = items, scrollState = scrollState)
     }
 }
---
+++ app/src/main/java/br/com/fiap/myaiteacher/ui/screen/chat/components/FooterChat.kt
@@ -0,0 +1,88 @@
+package br.com.fiap.myaiteacher.ui.screen.chat.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.fiap.myaiteacher.R
+import br.com.fiap.myaiteacher.ui.screen.chat.ChatScreenViewModel
+import br.com.fiap.myaiteacher.ui.theme.Montserrat
+
+@Composable
+fun FooterChat(configuration: Configuration, comment: String, chatScreenViewModel: ChatScreenViewModel, items: SnapshotStateList<String>, scrollState: LazyListState) {
+    Row(
+        modifier = Modifier
+            .background(color = Color(0xFF2C2F34))
+            .height((configuration.screenHeightDp * 0.13).dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        OutlinedTextField(
+            value = comment,
+            onValueChange = {
+                chatScreenViewModel.onCommentChanged(it)
+            },
+            placeholder = {
+                Text(text = "What’s your question? ",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = Montserrat,
+                        fontWeight = FontWeight(400),
+                        color = Color(0xB2FFFFFF),
+                    )
+                )
+            },
+            modifier = Modifier
+                .width(287.dp)
+                .height(55.dp)
+                .padding(start = 22.dp),
+            shape = RoundedCornerShape(
+                12.dp
+            ),
+            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences)
+        )
+        IconButton(
+            onClick = {
+                chatScreenViewModel.onAddNewComment(comment)
+                chatScreenViewModel.onCommentChanged("")
+
+            },
+            modifier = Modifier
+                .padding(start = 30.dp, end = 10.dp)
+                .width(35.dp)
+                .height(35.dp)
+        )
+        {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_send_24),
+                contentDescription = "Send message",
+                tint = Color(0xB2FFFFFF)
+            )
+            LaunchedEffect(items) {
+                scrollState.animateScrollToItem(if (items.size > 0) items.size -1 else 0)
+            }
+        }
+    }
+}
